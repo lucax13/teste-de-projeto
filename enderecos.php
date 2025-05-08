d80b15691c8919154eb8dde0f6fedc1dfb13d481
lucax13 <lucas13lima2006@gmail.com>
2025-05-08 11:38:01 -0300

    rodape

--- enderecos.php
+++ enderecos.php
@@ -30,9 +30,11 @@
     </header>
 
 
-    <footer>
-        <p>Explosão Cultural tem Permisão ao uso de cada imagen</p>
+    
+    <footer class="bg-black text-center py-3">
+      <p class="m-0">Explosão Cultural — Imagens usadas com permissão</p>
     </footer>
+
 
     <script src="/js/menu.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
