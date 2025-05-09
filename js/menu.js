2d80315a9f3fa2907043800fc281cca9e725bb8b
lucax13 <lucas13lima2006@gmail.com>
2025-05-09 08:52:32 -0300

    adicionando eventos

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const btn = document.getElementById('menuBtn');
-    const menu = document.getElementById('menuNav');
-  
-    btn.addEventListener('click', function () {
-      menu.classList.toggle('show');
-    });
+  const btn = document.getElementById('menuBtn');
+  const menu = document.getElementById('menuNav');
+
+  btn.addEventListener('click', function () {
+    menu.classList.toggle('show');
   });
-  
+});
